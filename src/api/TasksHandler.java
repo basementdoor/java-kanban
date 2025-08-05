1053fa51e45beae009c213cd2f7b17ce23188faa
Egor Ilyin <basementdoor@yandex.com>
2025-08-06 00:08:31 +0300

    fix whitespace codestyle

--- src/api/TasksHandler.java
+++ src/api/TasksHandler.java
@@ -2,7 +2,7 @@ package api;
 
 import manager.TaskManager;
 
-public class TasksHandler extends  BaseHttpHandler{
+public class TasksHandler extends BaseHttpHandler {
 
     public TasksHandler(TaskManager manager) {
         super(manager);
